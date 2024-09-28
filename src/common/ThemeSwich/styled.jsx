dd733e5d96a93c22153f61d38b327ed468e5af1d
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-09-28 18:54:59 +0200

    Styled for ThemeSwitch added in styled.js

---
+++ src/common/ThemeSwich/styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { ReactComponent as SunIcon} from "./sun.svg";
+
+export const Wrapper = styled.div``;
+export const Box = styled.div``;
+export const Button = styled.button``;
+export const Text = styled.span``;
+export const IconBox = styled.div``;
+export const Icon = styled(SunIcon)``;
