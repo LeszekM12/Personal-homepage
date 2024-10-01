d5c6832789711bc198c16cb08ae745f23d611306
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-10-01 20:53:02 +0200

    added theme colors names in core

--- src/core/theme.jsx
+++ src/core/theme.jsx
@@ -1,5 +1,21 @@
 const colorNames = {
-
+    black: "#252525",
+    mineshaftLighter: "#313131",
+    tundora: "#414141",
+    lightGray: "#6e7e9144",
+    primaryAlpha: "#0366d836",
+    dodgerBlueAlpha: "#2188FF56",
+    blumine: "#1F5189",
+    scienceBlue: "#0366D6",
+    slateGray: "#6E7E91",
+    shipCove: "#6D93BE",
+    dodgerBlue: "#2188FF",
+    anakiwa: "#8CC2FF",
+    tropicalBlue: "#CDE0F7",
+    porcelain: "#F2F3F4",
+    mercury: "#e5e5e5",
+    whiteLilac: "#FBFBFE",
+    white: "#FFFFFF",
 };
 
 export const themeDark = {
