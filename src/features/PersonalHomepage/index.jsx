695b4c9b910885863e38149187ccb8e4d547f2ab
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-10-22 18:25:28 +0200

    Implementing Skills in features  PersonalHomepage index

--- src/features/PersonalHomepage/index.jsx
+++ src/features/PersonalHomepage/index.jsx
@@ -1,10 +1,16 @@
 import {Container} from "./styled";
 import {ThemeSwitch} from "../../common/ThemeSwich";
 import {MainBio} from "./MainBio";
+import {Skills} from "./Skills";
+import {skills, nextSkills} from "./Skills/skillsData";
 
 export const PersonalHomepage = () => (
     <Container>
         <ThemeSwitch/>
         <MainBio/>
+        <main>
+            <Skills title={"My skill set includes 🛠️"} skills={skills}/>
+            <Skills title={"What I want to learn next 🚀"} skills={nextSkills}/>
+        </main>
     </Container>
 );
